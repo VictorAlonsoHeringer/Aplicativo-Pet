d8ef188536bde106302bb7e7c6ba6a048d35d033
andreWill99 <andrewilliam.cbpm@gmail.com>
2024-11-20 12:58:25 -0300

    corrigido posição dos itens

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbee141d8e09be8edd60ba5dca465f973dee066e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2a7bdbd9b51ceee11d20d0a3294c503a630577c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
